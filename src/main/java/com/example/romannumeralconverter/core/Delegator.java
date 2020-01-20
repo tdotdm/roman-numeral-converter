577e69269dc98c22f6e5b4f748c256fbd1155155
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 21:47:14 +0000

    Split logic between Delegator and StartupListener

--- src/main/java/com/example/romannumeralconverter/core/Delegator.java
+++ src/main/java/com/example/romannumeralconverter/core/Delegator.java
@@ -1,7 +1,9 @@
 package com.example.romannumeralconverter.core;
 
-import com.example.romannumeralconverter.core.configuration.ApplicationProperties;
-import com.example.romannumeralconverter.core.numeral.*;
+import com.example.romannumeralconverter.core.numeral.NumeralValidator;
+import com.example.romannumeralconverter.core.numeral.RomanNumeralGenerator;
+import com.example.romannumeralconverter.core.numeral.RomanNumeralResultPrinter;
+import com.example.romannumeralconverter.core.numeral.StringNumeralToIntegerNumeralConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,35 +14,26 @@ import java.util.Optional;
 @Slf4j
 @RequiredArgsConstructor
 public class Delegator {
-    private final ApplicationProperties applicationProperties;
-    private final UserInputFormatter userInputFormatter;
     private final StringNumeralToIntegerNumeralConverter stringNumeralToIntegerNumeralConverter;
     private final NumeralValidator numeralValidator;
     private final RomanNumeralGenerator romanNumeralGenerator;
     private final RomanNumeralResultPrinter romanNumeralResultPrinter;
 
-    public void delegate() {
-        final String numberToConvert = applicationProperties.getNumberToConvert();
-        final Optional<String> optionalFormattedUserInput = userInputFormatter.format(numberToConvert);
-        if (optionalFormattedUserInput.isPresent()) {
-            final String formattedUserInput = optionalFormattedUserInput.get();
-            final Optional<Integer> optionalNumeral = stringNumeralToIntegerNumeralConverter.convert(formattedUserInput);
-            if (optionalNumeral.isPresent()) {
-                final Integer numeral = optionalNumeral.get();
-                final boolean numeralIsValid = numeralValidator.numeralIsValid(numeral);
+    public void delegate(final String userInput) {
+        final Optional<Integer> optionalNumberToConvert = stringNumeralToIntegerNumeralConverter.convert(userInput);
+        if (optionalNumberToConvert.isPresent()) {
+            final Integer numberToConvert = optionalNumberToConvert.get();
+            final boolean numberIsValid = numeralValidator.numeralIsValid(numberToConvert);
 
-                if (numeralIsValid) {
-                    log.info("Converting {} to Roman numeral.", numeral);
-                    final String numeralAsRomanNumeral = romanNumeralGenerator.generate(numeral);
-                    romanNumeralResultPrinter.print(numeralAsRomanNumeral);
-                } else {
-                    log.error("Number is not valid. ");
-                }
+            if (numberIsValid) {
+                log.info("Converting {} to Roman numeral.", numberToConvert);
+                final String romanNumeral = romanNumeralGenerator.generate(numberToConvert);
+                romanNumeralResultPrinter.print(romanNumeral);
             } else {
-                log.error("Cannot convert User Input to Integer.");
+                log.error("Number is not valid.");
             }
         } else {
-            log.error("Cannot format User Input.");
+            log.error("Cannot convert {} to Integer.", userInput);
         }
     }
 }
--- src/main/java/com/example/romannumeralconverter/core/StartupListener.java
+++ src/main/java/com/example/romannumeralconverter/core/StartupListener.java
@@ -1,18 +1,30 @@
 package com.example.romannumeralconverter.core;
 
+import com.example.romannumeralconverter.core.configuration.ApplicationProperties;
+import com.example.romannumeralconverter.core.numeral.UserInputFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class StartupListener implements CommandLineRunner {
+    private final ApplicationProperties applicationProperties;
+    private final UserInputFormatter userInputFormatter;
     private final Delegator delegator;
 
     @Override
     public void run(final String... args) {
-        delegator.delegate();
+        final String numberToConvert = applicationProperties.getNumberToConvert();
+        final Optional<String> optionalFormattedUserInput = userInputFormatter.format(numberToConvert);
+
+        if (optionalFormattedUserInput.isPresent()) {
+            final String userInput = optionalFormattedUserInput.get();
+            delegator.delegate(userInput);
+        }
     }
 }
