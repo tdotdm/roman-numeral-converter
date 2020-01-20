632999fb4203fa60d4797a951cc8f15c488af1dc
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 23:20:26 +0000

    Collect list of PlaceValues in RomanNumeralGeneratorImpl

--- src/main/java/com/example/romannumeralconverter/core/numeral/RomanNumeralGeneratorImpl.java
+++ src/main/java/com/example/romannumeralconverter/core/numeral/RomanNumeralGeneratorImpl.java
@@ -1,6 +1,10 @@
 package com.example.romannumeralconverter.core.numeral;
 
+import com.example.romannumeralconverter.core.domain.*;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class RomanNumeralGeneratorImpl implements RomanNumeralGenerator {
@@ -10,6 +14,28 @@ public class RomanNumeralGeneratorImpl implements RomanNumeralGenerator {
             throw new IllegalArgumentException("Cannot convert number to Roman numeral!");
         }
 
-        return null;
+        final List<PlaceValue> placeValues = new ArrayList<>();
+        final int numberLength = String.valueOf(number).length();
+        if (numberLength > 3) {
+            final PlaceValue placeValue = ThousandPlaceValue.from(number);
+            placeValues.add(placeValue);
+        }
+
+        if (numberLength > 2) {
+            final PlaceValue placeValue = HundredPlaceValue.from(number);
+            placeValues.add(placeValue);
+        }
+
+        if (numberLength > 1) {
+            final PlaceValue placeValue = TenPlaceValue.from(number);
+            placeValues.add(placeValue);
+        }
+
+        if (numberLength > 0) {
+            final PlaceValue placeValue = UnitPlaceValue.from(number);
+            placeValues.add(placeValue);
+        }
+
+        return "todo";
     }
 }
