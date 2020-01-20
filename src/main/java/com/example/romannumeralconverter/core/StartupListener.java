585e9b30aaa7acf73fa7b16bd468161e4cca9a1e
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 21:24:14 +0000

    Add validator to StartupListener

--- src/main/java/com/example/romannumeralconverter/core/StartupListener.java
+++ src/main/java/com/example/romannumeralconverter/core/StartupListener.java
@@ -2,12 +2,15 @@ package com.example.romannumeralconverter.core;
 
 import com.example.romannumeralconverter.core.configuration.ApplicationProperties;
 import com.example.romannumeralconverter.core.numeral.NumeralFormatter;
+import com.example.romannumeralconverter.core.numeral.NumeralValidator;
 import com.example.romannumeralconverter.core.numeral.RomanNumeralGenerator;
 import com.example.romannumeralconverter.core.numeral.StringNumeralToIntegerNumeralConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @Slf4j
@@ -16,20 +19,30 @@ public class StartupListener implements CommandLineRunner {
     private final ApplicationProperties applicationProperties;
     private final NumeralFormatter numeralFormatter;
     private final StringNumeralToIntegerNumeralConverter stringNumeralToIntegerNumeralConverter;
+    private final NumeralValidator numeralValidator;
     private final RomanNumeralGenerator romanNumeralGenerator;
 
     @Override
     public void run(final String... args) {
         final String numberToConvert = applicationProperties.getNumberToConvert();
         final String formattedNumberToConvert = numeralFormatter.format(numberToConvert);
-        stringNumeralToIntegerNumeralConverter
-                .convert(formattedNumberToConvert)
-                .ifPresentOrElse(numeral -> {
-                    log.info("Converting {} to Roman Numeral.", numeral);
-                    final String numeralAsRomanNumeral = romanNumeralGenerator.generate(numeral);
-                    log.info("******************************");
-                    log.info("{} is {} in Roman numerals.", numeral, numeralAsRomanNumeral);
-                    log.info("******************************");
-                }, () -> log.error("Application shutting down - cannot convert to Roman Numeral."));
+        final Optional<Integer> optionalNumeral = stringNumeralToIntegerNumeralConverter.convert(formattedNumberToConvert);
+
+        if (optionalNumeral.isPresent()) {
+            final Integer numeral = optionalNumeral.get();
+            final boolean numeralIsValid = numeralValidator.numeralIsValid(numeral);
+
+            if (numeralIsValid) {
+                log.info("Converting {} to Roman numeral.", numeral);
+                final String numeralAsRomanNumeral = romanNumeralGenerator.generate(numeral);
+                log.info("******************************");
+                log.info("{} is '{}' in Roman numerals.", numeral, numeralAsRomanNumeral);
+                log.info("******************************");
+            } else {
+                log.error("Number is not valid. ");
+            }
+        } else {
+            log.error("Cannot convert {} to Integer.", formattedNumberToConvert);
+        }
     }
 }
