669b8ce5ffc64a536ebeb7dfdef7095464b4fa30
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 21:10:28 +0000

    Add NumeralFormatter and StringNumeralToIntegerNumeralConverter to StartupListener

--- src/main/java/com/example/romannumeralconverter/core/StartupListener.java
+++ src/main/java/com/example/romannumeralconverter/core/StartupListener.java
@@ -1,14 +1,29 @@
 package com.example.romannumeralconverter.core;
 
+import com.example.romannumeralconverter.core.configuration.ApplicationProperties;
+import com.example.romannumeralconverter.core.numeral.NumeralFormatter;
+import com.example.romannumeralconverter.core.numeral.StringNumeralToIntegerNumeralConverter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class StartupListener implements CommandLineRunner {
+    private final ApplicationProperties applicationProperties;
+    private final NumeralFormatter numeralFormatter;
+    private final StringNumeralToIntegerNumeralConverter stringNumeralToIntegerNumeralConverter;
+
     @Override
-    public void run(final String... args) throws Exception {
-        log.info("Hello, world!");
+    public void run(final String... args) {
+        final String numberToConvert = applicationProperties.getNumberToConvert();
+        final String formattedNumberToConvert = numeralFormatter.format(numberToConvert);
+        stringNumeralToIntegerNumeralConverter
+                .convert(formattedNumberToConvert)
+                .ifPresentOrElse(numeral -> {
+                    log.info("Converting {} to Roman Numeral.", numeral);
+                }, () -> log.error("Application shutting down - cannot convert to Roman Numeral."));
     }
 }
