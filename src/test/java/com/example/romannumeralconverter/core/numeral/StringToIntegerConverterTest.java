c1190718ca455a5327b3850441e5cede3b4b0711
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 22:14:39 +0000

    Add Target to StringToIntegerConverterTest

--- src/test/java/com/example/romannumeralconverter/core/numeral/StringToIntegerConverterTest.java
+++ src/test/java/com/example/romannumeralconverter/core/numeral/StringToIntegerConverterTest.java
@@ -1,5 +1,8 @@
 package com.example.romannumeralconverter.core.numeral;
 
 class StringToIntegerConverterTest {
-
+    /*
+     * Test subject.
+     * */
+    private StringToIntegerConverter target = new StringToIntegerConverter();
 }
