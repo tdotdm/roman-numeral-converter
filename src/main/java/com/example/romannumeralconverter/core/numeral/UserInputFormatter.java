f6096f2ce4e71a19bd66369dd4b2ba1d666669b1
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 21:33:17 +0000

    Update UserInputFormatter to return Optional

--- src/main/java/com/example/romannumeralconverter/core/numeral/UserInputFormatter.java
+++ src/main/java/com/example/romannumeralconverter/core/numeral/UserInputFormatter.java
@@ -3,14 +3,21 @@ package com.example.romannumeralconverter.core.numeral;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @Slf4j
 public class UserInputFormatter {
     private static final String LEADING_ZEROES = "^0+(?!$)";
 
-    public String format(final String numeral) {
-        log.info("Removing leading zeroes from numeral.");
+    public Optional<String> format(final String numeral) {
+        if (numeral.isEmpty()) {
+            log.error("No valid User Input to format.");
+            return Optional.empty();
+        }
 
-        return numeral.replaceFirst(LEADING_ZEROES, "");
+        log.info("Formatting User Input.");
+        final String formattedUserInput = numeral.replaceFirst(LEADING_ZEROES, "");
+        return Optional.of(formattedUserInput);
     }
 }
