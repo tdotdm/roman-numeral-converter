52ed4b9d54d1fd3ab1adfdc8a1f0b659a6a45dfc
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 21:09:26 +0000

    Remove requiredargsconstructor annotation from NumeralValidator

--- src/main/java/com/example/romannumeralconverter/core/numeral/NumeralValidator.java
+++ src/main/java/com/example/romannumeralconverter/core/numeral/NumeralValidator.java
@@ -1,13 +1,11 @@
 package com.example.romannumeralconverter.core.numeral;
 
 import com.example.romannumeralconverter.core.configuration.ApplicationProperties;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-@RequiredArgsConstructor
 public class NumeralValidator {
     private final int lowerBound;
     private final int upperBound;
