5cb91c5fe891f395eb0aae82d1c5a2227baca8ab
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 23:51:23 +0000

    Implement getRomanNumeral method in TenPlaceValue

--- src/main/java/com/example/romannumeralconverter/core/domain/TenPlaceValue.java
+++ src/main/java/com/example/romannumeralconverter/core/domain/TenPlaceValue.java
@@ -14,6 +14,39 @@ public class TenPlaceValue extends PlaceValue {
 
     @Override
     public String getRomanNumeral() {
-        return null;
+        final StringBuilder stringBuilder = new StringBuilder();
+        final String xKey = RomanNumeral.X.getKey();
+        final String lKey = RomanNumeral.L.getKey();
+        final String cKey = RomanNumeral.C.getKey();
+
+        if (this.value < 4) {
+            for (int i = 0; i < this.value; i++) {
+                stringBuilder.append(xKey);
+            }
+        } else if (this.value == 4) {
+            stringBuilder.append(xKey);
+            stringBuilder.append(lKey);
+        } else if (this.value == 5) {
+            stringBuilder.append(lKey);
+        } else if (this.value == 6) {
+            stringBuilder.append(lKey);
+            stringBuilder.append(xKey);
+        } else if (this.value == 7) {
+            stringBuilder.append(lKey);
+            stringBuilder.append(xKey);
+            stringBuilder.append(xKey);
+        } else if (this.value == 8) {
+            stringBuilder.append(lKey);
+            stringBuilder.append(xKey);
+            stringBuilder.append(xKey);
+            stringBuilder.append(xKey);
+        } else if (this.value == 9) {
+            stringBuilder.append(xKey);
+            stringBuilder.append(cKey);
+        } else if (this.value == 10) {
+            stringBuilder.append(cKey);
+        }
+
+        return stringBuilder.toString();
     }
 }
