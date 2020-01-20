2677835f3c5d2a583deb47482faa392b6dd62876
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 21:16:49 +0000

    Create RomanNumeralGeneratorImpl

---
+++ src/main/java/com/example/romannumeralconverter/core/numeral/RomanNumeralGeneratorImpl.java
@@ -0,0 +1,11 @@
+package com.example.romannumeralconverter.core.numeral;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RomanNumeralGeneratorImpl implements RomanNumeralGenerator {
+    @Override
+    public String generate(final int number) {
+        return null;
+    }
+}
