5d98be3420b9d7291b7dd986c2ce03d23251238f
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 23:22:14 +0000

    Change members in PlaceValue to be protected

--- src/main/java/com/example/romannumeralconverter/core/domain/PlaceValue.java
+++ src/main/java/com/example/romannumeralconverter/core/domain/PlaceValue.java
@@ -1,12 +1,14 @@
 package com.example.romannumeralconverter.core.domain;
 
 public abstract class PlaceValue {
-    private final Rank rank; //i.e. Thousands
-    private final Integer value; //i.e. 3
+    protected final Rank rank; //i.e. Thousands
+    protected final Integer value; //i.e. 3
 
     public PlaceValue(final Rank rank,
                       final Integer value) {
         this.rank = rank;
         this.value = value;
     }
+
+    abstract String getRomanNumeral();
 }
