93d8e435ea715dfb8d62f8f7c997c4fa28b76c9d
Darryn McGaw <dm.maxton@gmail.com>
2020-01-20 21:57:10 +0000

    Add target to NumberValidatorTest

--- src/test/java/com/example/romannumeralconverter/core/numeral/NumberValidatorTest.java
+++ src/test/java/com/example/romannumeralconverter/core/numeral/NumberValidatorTest.java
@@ -1,5 +1,19 @@
 package com.example.romannumeralconverter.core.numeral;
 
-class NumberValidatorTest {
+import com.example.romannumeralconverter.core.configuration.ApplicationProperties;
+import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+
+class NumberValidatorTest {
+    /*
+     * Target
+     * */
+    private final ApplicationProperties applicationProperties = mock(ApplicationProperties.class);
+    private final NumberValidator target = new NumberValidator(applicationProperties);
+
+    @Test
+    public void testName() {
+        //ignore
+    }
 }
