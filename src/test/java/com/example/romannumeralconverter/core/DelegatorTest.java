1de7ee8d8fcf3bd8926e6be759253f74e31db8b3
Darryn McGaw <dm.maxton@gmail.com>
2020-01-21 22:15:59 +0000

    Add delegate_ShouldThrowException_WhenGivenEmptyString test to DelegatorTest

--- src/test/java/com/example/romannumeralconverter/core/DelegatorTest.java
+++ src/test/java/com/example/romannumeralconverter/core/DelegatorTest.java
@@ -5,9 +5,14 @@ import com.example.romannumeralconverter.core.domain.request.RequestValidator;
 import com.example.romannumeralconverter.core.domain.request.StringToIntegerConverter;
 import com.example.romannumeralconverter.core.domain.romannumeral.RomanNumeralGenerator;
 import com.example.romannumeralconverter.core.domain.romannumeral.RomanNumeralGeneratorImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class DelegatorTest {
     /*
@@ -24,10 +29,25 @@ class DelegatorTest {
             romanNumeralGenerator
     );
 
-    @Test
-    public void testName() {
-        //given
-        //when
+    private void assertExceptionThrown(final String input) {
         //then
+        Assertions.assertThatThrownBy(() -> {
+            target.delegate(input);
+        }).isInstanceOf(ServiceException.class);
+    }
+
+    private void givenRequestFormatterReturnsEmptyOptional() {
+        //given
+        when(requestFormatter.format(any())).thenReturn(Optional.empty());
+    }
+
+    @Test
+    public void delegate_ShouldThrowException_WhenGivenEmptyString() {
+        //given
+        givenRequestFormatterReturnsEmptyOptional();
+        final String input = "     ";
+
+        //then
+        assertExceptionThrown(input);
     }
 }
