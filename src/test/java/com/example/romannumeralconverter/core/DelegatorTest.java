6e9950b5ff9410c9b2b9d3ca418f34dba68fb300
Darryn McGaw <dm.maxton@gmail.com>
2020-01-21 22:09:55 +0000

    Add target to DelegatorTest

--- src/test/java/com/example/romannumeralconverter/core/DelegatorTest.java
+++ src/test/java/com/example/romannumeralconverter/core/DelegatorTest.java
@@ -1,5 +1,33 @@
 package com.example.romannumeralconverter.core;
 
-class DelegatorTest {
+import com.example.romannumeralconverter.core.domain.request.RequestFormatter;
+import com.example.romannumeralconverter.core.domain.request.RequestValidator;
+import com.example.romannumeralconverter.core.domain.request.StringToIntegerConverter;
+import com.example.romannumeralconverter.core.domain.romannumeral.RomanNumeralGenerator;
+import com.example.romannumeralconverter.core.domain.romannumeral.RomanNumeralGeneratorImpl;
+import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+
+class DelegatorTest {
+    /*
+     * Test subject.
+     * */
+    private final StringToIntegerConverter stringToIntegerConverter = mock(StringToIntegerConverter.class);
+    private final RequestFormatter requestFormatter = mock(RequestFormatter.class);
+    private final RequestValidator requestValidator = mock(RequestValidator.class);
+    private final RomanNumeralGenerator romanNumeralGenerator = mock(RomanNumeralGeneratorImpl.class);
+    private final Delegator target = new Delegator(
+            stringToIntegerConverter,
+            requestFormatter,
+            requestValidator,
+            romanNumeralGenerator
+    );
+
+    @Test
+    public void testName() {
+        //given
+        //when
+        //then
+    }
 }
