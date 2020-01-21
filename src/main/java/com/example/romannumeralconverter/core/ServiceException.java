d6f328a84f06b394e16a7bd304c5c98b0c490294
Darryn McGaw <dm.maxton@gmail.com>
2020-01-21 20:49:49 +0000

    Create ServiceException

---
+++ src/main/java/com/example/romannumeralconverter/core/ServiceException.java
@@ -0,0 +1,7 @@
+package com.example.romannumeralconverter.core;
+
+public class ServiceException extends Exception {
+    public ServiceException(final String message) {
+        super(message);
+    }
+}
