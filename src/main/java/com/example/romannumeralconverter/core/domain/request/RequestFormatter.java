b2bb0e5e009ea24ba28752061baca6e4dd7bcc32
Darryn McGaw <dm.maxton@gmail.com>
2020-01-21 21:25:36 +0000

    Add comment to RequestFormatter

--- src/main/java/com/example/romannumeralconverter/core/domain/request/RequestFormatter.java
+++ src/main/java/com/example/romannumeralconverter/core/domain/request/RequestFormatter.java
@@ -10,6 +10,11 @@ import java.util.Optional;
 public class RequestFormatter {
     private static final String LEADING_ZEROES = "^0+(?!$)";
 
+    /*
+     * Format the User's input by
+     * removing all leading zeroes.
+     * For example, 007 will become 7.
+     * */
     public Optional<String> format(final String numberToConvert) {
         if (numberToConvert == null || numberToConvert.isBlank()) {
             return Optional.empty();
