7c4d52efbc25343f6db5b579b405faa92deb75dc
Darryn McGaw <dm.maxton@gmail.com>
2020-01-21 21:37:50 +0000

    Add generate_ShouldReturnExpectedStrings_WhenGivenUnits method to RomanNumeralGeneratorImplTest

--- src/test/java/com/example/romannumeralconverter/core/domain/romannumeral/RomanNumeralGeneratorImplTest.java
+++ src/test/java/com/example/romannumeralconverter/core/domain/romannumeral/RomanNumeralGeneratorImplTest.java
@@ -2,6 +2,9 @@ package com.example.romannumeralconverter.core.domain.romannumeral;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RomanNumeralGeneratorImplTest {
@@ -19,5 +22,22 @@ class RomanNumeralGeneratorImplTest {
         assertThatThrownBy(() -> {
             target.generate(input);
         });
+    }
+
+    @Test
+    public void generate_ShouldReturnExpectedStrings_WhenGivenUnits() {
+        //given
+        final List<Integer> inputs = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        final List<String> outputs = List.of("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X");
+
+        //when
+        final int inputsSize = inputs.size();
+        for (int x = 0; x < inputsSize; x++) {
+            final String result = target.generate(inputs.get(x));
+            final String expectedResult = outputs.get(x);
+
+            //then
+            assertThat(result).isEqualTo(expectedResult);
+        }
     }
 }
