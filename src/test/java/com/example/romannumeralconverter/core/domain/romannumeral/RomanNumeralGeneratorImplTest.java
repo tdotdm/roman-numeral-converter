787ffc03968a469d404b4c191e4dd18db46a9e64
Darryn McGaw <dm.maxton@gmail.com>
2020-01-21 21:30:00 +0000

    Add target to RomanNumeralGeneratorImplTest

--- src/test/java/com/example/romannumeralconverter/core/domain/romannumeral/RomanNumeralGeneratorImplTest.java
+++ src/test/java/com/example/romannumeralconverter/core/domain/romannumeral/RomanNumeralGeneratorImplTest.java
@@ -1,5 +1,17 @@
 package com.example.romannumeralconverter.core.domain.romannumeral;
 
-class RomanNumeralGeneratorImplTest {
+import org.junit.jupiter.api.Test;
 
+class RomanNumeralGeneratorImplTest {
+    /*
+     * Test subject.
+     * */
+    private final RomanNumeralGeneratorImpl target = new RomanNumeralGeneratorImpl();
+
+    @Test
+    public void testName() {
+        //given
+        //when
+        //then
+    }
 }
